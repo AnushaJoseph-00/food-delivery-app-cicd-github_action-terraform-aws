92b337f101e74c5459e62bad237cb94324521a8b
AnushaJoseph-00 <anushajoe805@gmail.com>
2026-08-04 19:17:17 +1200

    Add GitHub Actions CI/CD pipeline

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,12 +2,12 @@ terraform {
     required_providers {
         aws = {
             source  = "hashicorp/aws"
-            version = "~> 6.0"
+            version = "~> 5.0"
         }
     }
     required_version = ">= 1.5.0"
 }
 
 provider "aws" {
-    region = var.region
+  region = var.region
 }
--- terraform/securitygroup.tf
+++ terraform/securitygroup.tf
@@ -1,10 +1,10 @@
 #ALB Security Group
 resource "aws_security_group" "alb_sg" {
-    name        = "${var.project_name}-alb-sg"
-    description = "Security group for ALB"
-    vpc_id      = aws_vpc.main.id
-    
-     ingress {
+  name        = "${var.project_name}-alb-sg"
+  description = "Security group for ALB"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
     description = "HTTP frontend"
     from_port   = 80
     to_port     = 80
@@ -44,27 +44,27 @@ resource "aws_security_group" "alb_sg" {
 
 #ECS Security Group
 resource "aws_security_group" "ecs_sg" {
-    name        = "${var.project_name}-ecs-sg"
-    description = "Security group for ECS tasks"
-    vpc_id      = aws_vpc.main.id
-    
-    ingress {
-        description      = "Allow traffic from ALB"
-        from_port        = var.backend_port
-        to_port          = var.backend_port        
-        protocol         = "tcp"
-        security_groups  = [aws_security_group.alb_sg.id]
-    }
+  name        = "${var.project_name}-ecs-sg"
+  description = "Security group for ECS tasks"
+  vpc_id      = aws_vpc.main.id
 
-    egress {
-        description      = "Allow all outbound traffic"
-        from_port        = 0
-        to_port          = 0
-        protocol         = "-1"
-        cidr_blocks      = ["0.0.0.0/0"]
-    }
-    tags = {
-        Name    = "${var.project_name}-ecs-sg"
-        Project = var.project_name
-    }
+  ingress {
+    description     = "Allow traffic from ALB"
+    from_port       = var.backend_port
+    to_port         = var.backend_port
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name    = "${var.project_name}-ecs-sg"
+    Project = var.project_name
+  }
 }
